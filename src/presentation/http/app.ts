3eca4dd7a273cce3d87441c635c6861a709563a9
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2024-04-30 10:32:31 -0300

    feat: add custom error in when route not found

--- src/presentation/http/app.ts
+++ src/presentation/http/app.ts
@@ -1,8 +1,19 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 
+import { InternalServerError, NotFoundError } from "@/shared/error";
 import { eventsRouter } from "@presentation/http/routes";
 
 export const app = express();
 
 app.use(express.json());
 app.use('/events', eventsRouter)
+
+app.use((req, res, next) => {
+  const error = new NotFoundError("route not found");
+  next(error);
+})
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof NotFoundError) {
+    return res.status(error.statusCode).send({ error: error.message })
+  }
+});
