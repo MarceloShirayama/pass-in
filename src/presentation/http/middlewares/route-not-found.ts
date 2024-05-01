c36ac28bdd6a7929077a116db34273f3cd630fad
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2024-05-01 20:58:35 -0300

    refactor: remove unnecessary generic type

--- src/presentation/http/middlewares/route-not-found.ts
+++ src/presentation/http/middlewares/route-not-found.ts
@@ -2,7 +2,7 @@ import { Express } from "express";
 
 import { NotFoundError } from "@/shared/error";
 
-export function routeNotFoundMiddleware<T>(app: Express) {
+export function routeNotFoundMiddleware(app: Express) {
   app.use((req, res, next) => {
     const error = new NotFoundError("route not found");
     next(error);
